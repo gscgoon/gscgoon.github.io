9a95735913f0fd392451f76ed34a6d844e56deca
gscgoon <gscgoon@163.com>
2021-12-05 23:13:32 +0800

    Site updated: 2021-12-05 23:13:30

--- aplayer/ap.js
+++ aplayer/ap.js
@@ -1,13 +1,19 @@
 const ap = new APlayer({
   container: document.getElementById('aplayer'),
-  lrcType: 1,
+  lrcType: 3,
   listFolded: true,
   audio: [
     {
       name: '光辉岁月',
       artist: 'Beyond',
-      url: '/aplayer/url/Beyond-光辉岁月 mp3免费下载.mp3',
-      lrc: "[ti:光辉岁月][ar:BEYOND][al:Control][by:][offset:0][00:00.00]光辉岁月 - BEYOND[00:09.54]词：黄家驹[00:19.08]曲：黄家驹[00:28.62]钟声响起归家的讯号[00:31.53][00:33.07]在他生命里[00:34.74][00:36.29]仿佛带点唏嘘[00:39.68][00:41.58]黑色肌肤给他的意义"
+      url: '/aplayer/url/光辉岁月.mp3',
+      lrc: "/aplayer/lrc/光辉岁月.lrc"
+    },
+	{
+      name: '不再犹豫',
+      artist: 'Beyond',
+      url: '/aplayer/url/不再犹豫.m4a',
+      lrc: "/aplayer/lrc/不再犹豫.lrc"
     }
   ]
 });
