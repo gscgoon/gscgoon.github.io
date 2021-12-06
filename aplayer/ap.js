04e78df400fb08c974135b5459e548c4a2c2e4bb
gscgoon <gscgoon@163.com>
2021-12-07 00:24:53 +0800

    Site updated: 2021-12-07 00:24:50

--- aplayer/ap.js
+++ aplayer/ap.js
@@ -6,12 +6,14 @@ const ap = new APlayer({
     {
       name: '光辉岁月',
       artist: 'Beyond',
+	  cover: '/aplayer/cover/光辉岁月.jpg',
       url: '/aplayer/url/光辉岁月.mp3',
       lrc: "/aplayer/lrc/光辉岁月.lrc"
     },
 	{
       name: '不再犹豫',
       artist: 'Beyond',
+	  cover: '/aplayer/cover/不再犹豫.jpg',
       url: '/aplayer/url/不再犹豫.m4a',
       lrc: "/aplayer/lrc/不再犹豫.lrc"
     }
